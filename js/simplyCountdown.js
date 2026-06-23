1048e19b0fe310ff0727a558041b89b14616758b
dziertech <dziertech@gmail.com>
2026-06-23 19:55:01 +0530

    countdown

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -115,10 +115,10 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2015,
-                month: 6,
-                day: 28,
-                hours: 0,
+                year: 2026,
+                month: 7,
+                day: 04,
+                hours: 12,
                 minutes: 0,
                 seconds: 0,
                 words: {
